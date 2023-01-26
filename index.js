43c284991cc2d1261805982b06061309bcac0eb5
YOUR NAME <ritter.christina.r.c@gmail.com>
2023-01-26 07:14:23 +0100

    darkmode updated

--- index.js
+++ index.js
@@ -1,21 +1,6 @@
 console.clear();
 
-const bodyElement = document.querySelector('[data-js="body"]');
-
-const darkMode = document.querySelector('[data-js="dark-mode-button"]');
-darkMode.addEventListener("click", () => {
-    bodyElement.classList.add("dark");
-    console.log("Switch to DarkMode");
-});
-
-const lightMode = document.querySelector('[data-js="light-mode-button"]');
-lightMode.addEventListener("click", () => {
-    bodyElement.classList.remove("dark");
-    console.log("Switch to lightMode");
-});
-
-const toggleMode = document.querySelector('[data-js="toggle-button"]');
-toggleMode.addEventListener("click", () => {
-    bodyElement.classList.toggle("dark");
-    console.log("Switch to toggleMode");
-});
+const icon = document.getElementById("switch");
+icon.onclick = function () {
+    document.body.classList.toggle("dark-theme")
+}
